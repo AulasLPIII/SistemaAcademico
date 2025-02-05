0571a8fb0f17fdf80d5456055ad93ca6f71b9335
Mauro Silva <maurosilva@ifma.edu.br>
2025-02-04 23:15:01 -0300

    Modelo de Dados

---
+++ Modelo de Dados/carga_disciplinas.sql
@@ -0,0 +1,17 @@
+SELECT * FROM bdacademicolp3.disciplina;
+
+insert into disciplina (sigla, nome, cargahoraria, modulo, tipo, eixotecnico)
+values
+('PRT','Língua Portuguesa',80,1,'Teórica',0);
+
+insert into disciplina (sigla, nome, cargahoraria, modulo, tipo, eixotecnico)
+values
+('SISOPER','Sistemas Operacionais',40,1,'Teórica',1);
+
+insert into disciplina (sigla, nome, cargahoraria, modulo, tipo, eixotecnico)
+values
+('PROEST','Programação Estruturada',80,2,'Teórica e Prática',1);
+
+insert into disciplina (sigla, nome, cargahoraria, modulo, tipo, eixotecnico)
+values
+('FISII','Física II',80,2,'Teórica',0);
