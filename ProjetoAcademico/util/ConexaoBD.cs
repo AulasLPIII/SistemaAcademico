a843021de42311b716f61f8b47ebadd4a958c513
Mauro Silva <maurosilva@ifma.edu.br>
2025-02-24 19:14:20 -0300

    Criação da Classe DisciplinaDAO

---
+++ ProjetoAcademico/util/ConexaoBD.cs
@@ -0,0 +1,28 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAcademico.util
+{
+    internal class ConexaoBD
+    {
+        private MySqlConnection con;
+        public ConexaoBD()
+        {
+            //Conexao com o Banco de Dados
+            string strconexao = "";
+            //String de Conexao com o Banco de Dados
+            strconexao = "server=localhost;userid=professor;password=professor@;database=bdcensopet";
+            //Criação do Canal de Comunicação
+            con = new MySqlConnection(strconexao);
+        }
+
+        public MySqlConnection RetornaConexao()
+        {
+            return this.con;
+        }
+    }
+}
